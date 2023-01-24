0c3cc2ff07253d0a864d2e7330f8ef95c63c31a0
Anubic29 <asitailo2039@gmail.com>
2023-01-24 17:34:57 +0200

    init: start rs-clone task

---
+++ rs-clone/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">RS Clone</h1>
+    </div>
+  );
+}
+
+export default App;
