2dfa774bdd984bbf97dc23cf62473c279c8fd354
Anubic29 <asitailo2039@gmail.com>
2023-02-01 22:17:41 +0200

    feat: add axios to frontend and add headers(allow cors) to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,16 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  next();
+});
+
 app.use("/api/users", users);
 
 async function start() {
--- rs-clone/src/App.tsx
+++ rs-clone/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import api from './api';
 
 import './App.scss';
 
+async function asReq() {
+  const response = await api.records.getData('users');
+  const data = response.data;
+  console.log(data);
+}
+
 function App() {
+  asReq();
   return (
     <div className="App">
       <h1 className="title">RS Clone</h1>
