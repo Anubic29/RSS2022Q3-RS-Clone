5ee1b1381a6397e4d76a6ce979046b186af4c19e
Vladyslav Tyshchenko <vvant97@gmail.com>
2023-02-06 13:52:42 +0200

    feat: add input and label components

---
+++ rs-clone/src/Components/Input/Input.tsx
@@ -0,0 +1,21 @@
+import Styles from './Input.module.scss';
+
+interface InputProps {
+  id: string;
+  type: 'text';
+  className?: string;
+  label?: string;
+  name?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  value?: string;
+}
+
+function Input(props: InputProps) {
+  const { className } = props;
+  const classNames = className ? `${Styles.Input} ${className}` : Styles.Input;
+
+  return <input {...props} className={classNames} />;
+}
+
+export default Input;
--- rs-clone/src/Pages/ProjectCreate/ProjectCreate.tsx
+++ rs-clone/src/Pages/ProjectCreate/ProjectCreate.tsx
@@ -1,9 +1,13 @@
+import Input from '../../Components/Input/Input';
+import Label from '../../Components/Label/Label';
 import Styles from './ProjectCreate.module.scss';
 
 function ProjectCreate() {
   return (
     <div className={Styles.ProjectCreate}>
       <h1>Create Project Template</h1>
+      <Label text="Project name" required htmlFor="project-name" />
+      <Input id="project-name" type="text" placeholder="Enter project name here" />
       <div className="row">
         <div className="col"></div>
         <div className="col"></div>
