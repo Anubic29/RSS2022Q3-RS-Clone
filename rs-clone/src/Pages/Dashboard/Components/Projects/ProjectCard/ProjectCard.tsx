3b3fbccad0a5f4b6d345468be8f2611320304174
Vladyslav Tyshchenko <vvant97@gmail.com>
2023-02-08 16:22:50 +0200

    fix: fix text rendering in project card

--- rs-clone/src/Pages/Dashboard/Components/Projects/ProjectCard/ProjectCard.tsx
+++ rs-clone/src/Pages/Dashboard/Components/Projects/ProjectCard/ProjectCard.tsx
@@ -1,7 +1,6 @@
-import { MdOutlineExpandMore } from 'react-icons/md';
+import { MdOutlineClear, MdArrowRightAlt } from 'react-icons/md';
 import { ProjectAvatarProps } from '../../../../../Components/ProjectAvatar/ProjectAvatar';
 import { ProjectAvatar } from '../../../../../Components';
-
 import Styles from './ProjectCard.module.scss';
 
 interface ProjectCardProps extends ProjectAvatarProps {
@@ -18,21 +17,27 @@ function ProjectCard(props: ProjectCardProps) {
   };
 
   return (
-    <li className={Styles.ProjectCard} style={cardStyles}>
-      <div className={Styles.TitleArea}>
-        <ProjectAvatar {...{ size, source, bgColor }} />
-        <div className={Styles.ProjectInfo}>
-          <a className={Styles.ProjectTitle}>{title}</a>
-          <p className={Styles.ProjectDescription}>{description}</p>
+    <>
+      <li className={Styles.ProjectCard} style={cardStyles}>
+        <div className={Styles.TitleArea}>
+          <ProjectAvatar className={Styles.Avatar} {...{ size, source, bgColor }} />
+          <div className={Styles.ProjectInfo}>
+            <a className={Styles.ProjectTitle}>{title}</a>
+            <p className={Styles.ProjectDescription}>{description}</p>
+          </div>
         </div>
-      </div>
-      <div className={Styles.ActionsArea}>
-        <div className={Styles.Actions}>
-          Actions
-          <MdOutlineExpandMore />
+        <div className={Styles.ActionsArea}>
+          <div className={Styles.Actions}>
+            Move to project
+            <MdArrowRightAlt />
+          </div>
+          <div className={Styles.Actions}>
+            Delete
+            <MdOutlineClear />
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </>
   );
 }
 
