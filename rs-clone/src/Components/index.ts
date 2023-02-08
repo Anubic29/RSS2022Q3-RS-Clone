e5558bdad71e5da4c59edc24a3ef3c17e706ee13
Vladyslav Tyshchenko <vvant97@gmail.com>
2023-02-08 20:35:44 +0200

    feat: add preloader

--- rs-clone/src/Components/index.ts
+++ rs-clone/src/Components/index.ts
@@ -5,5 +5,6 @@ import Input from './Input/Input';
 import Label from './Label/Label';
 import Button from './Button/Button';
 import InfoCard from './InfoCard/InfoCard';
+import Preloader from './Preloader/Preloader';
 
-export { ProjectAvatar, App, Input, Label, Button, InfoCard, EmptyData };
+export { ProjectAvatar, App, Input, Label, Button, InfoCard, EmptyData, Preloader };
