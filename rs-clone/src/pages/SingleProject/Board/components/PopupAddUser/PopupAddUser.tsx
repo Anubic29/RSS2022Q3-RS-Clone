c33e070b792df12952c63a8f11ca6a1aaa9de0e3
Anubic29 <asitailo2039@gmail.com>
2023-02-17 19:02:23 +0200

    feat: add opportunity to add user by name

--- rs-clone/src/pages/SingleProject/Board/components/PopupAddUser/PopupAddUser.tsx
+++ rs-clone/src/pages/SingleProject/Board/components/PopupAddUser/PopupAddUser.tsx
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { Input } from '../../../../../components';
 import { useBoard } from '../../../../../contexts/Board.context';
 import { useOverlay } from '../../../../../contexts';
-import { MdSearch } from 'react-icons/md';
+import { MdCancel, MdSearch } from 'react-icons/md';
 import { UserAvatar } from '../';
 import UserType from '../../../../../types/user/userType';
 import { convertLetterToHex } from '../../../../../utils/convertLetterToHex';
@@ -18,14 +18,10 @@ function PopupAddUser() {
   const [value, setValue] = useState('');
 
   useEffect(() => {
-    if (selectedUser) setValue(selectedUser.mail);
-  }, [selectedUser]);
-
-  useEffect(() => {
-    if (value.length > 0) {
+    if (value.trim().length > 0) {
       (async () => {
         const usersId = getUserList().map((data) => data._id);
-        const response = await api.users.getAllData(`?mail=${value}&limit=5`);
+        const response = await api.users.getAllData(`?value=${value}&limit=5`);
         if (response.status === 200) {
           const users = response.data.filter((user) => !usersId.includes(user._id));
           if (users.length === 1 && users[0].mail === value) setUserList([]);
@@ -33,12 +29,7 @@ function PopupAddUser() {
         } else setUserList([]);
       })();
     } else setUserList([]);
-    if (selectedUser && selectedUser.mail !== value) setSelectedUser(undefined);
   }, [value]);
-
-  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
 
   const onSubmitHandler = useCallback(() => {
     if (selectedUser) {
@@ -51,46 +42,68 @@ function PopupAddUser() {
     <div className={styles['pop-up']}>
       <div className={styles['pop-up__title']}>Add user to project</div>
       <div className={styles['pop-up__content']}>
-        <div className={styles['form']}>
-          <Input
-            value={value}
-            className={styles['input']}
-            id="add-user-input"
-            type="text"
-            required
-            onChange={onChangeHandler}
-            placeholder="Enter user mail"
-          />
-          <div className={styles['icon']}>
-            <MdSearch />
-          </div>
-          {userList.length > 0 && (
-            <div className={styles['user-list']}>
-              {userList.map((user) => (
-                <div
-                  className={styles['user-item']}
-                  key={user._id}
-                  onClick={() => setSelectedUser(user)}>
-                  <div className={styles['avatar']}>
-                    <UserAvatar
-                      content={user.firstName[0] + user.lastName[0]}
-                      color={`#${convertLetterToHex(user.firstName[0], 3, '9')}${convertLetterToHex(
-                        user.lastName[0],
-                        3,
-                        '9'
-                      )}`}
-                    />
-                  </div>
-                  {`${user.firstName} ${user.lastName}`}
-                </div>
-              ))}
+        {!selectedUser && (
+          <div className={styles['form']}>
+            <Input
+              value={value}
+              className={styles['input']}
+              id="add-user-input"
+              type="text"
+              required
+              onChange={(event) => setValue(event.target.value)}
+              placeholder="Enter user name / mail"
+            />
+            <div className={styles['icon']}>
+              <MdSearch />
             </div>
-          )}
-        </div>
+            {userList.length > 0 && (
+              <div className={styles['user-list']}>
+                {userList.map((user) => (
+                  <div
+                    className={styles['user-item']}
+                    key={user._id}
+                    onClick={() => setSelectedUser(user)}>
+                    <div className={styles['avatar']}>
+                      <UserAvatar
+                        content={user.firstName[0] + user.lastName[0]}
+                        color={`#${convertLetterToHex(
+                          user.firstName[0],
+                          3,
+                          '9'
+                        )}${convertLetterToHex(user.lastName[0], 3, '9')}`}
+                      />
+                    </div>
+                    {`${user.firstName} ${user.lastName}`}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        )}
+        {selectedUser && (
+          <div className={styles['selected-user-item']}>
+            <div className={styles['avatar']}>
+              <UserAvatar
+                content={selectedUser.firstName[0] + selectedUser.lastName[0]}
+                color={`#${convertLetterToHex(
+                  selectedUser.firstName[0],
+                  3,
+                  '9'
+                )}${convertLetterToHex(selectedUser.lastName[0], 3, '9')}`}
+              />
+            </div>
+            {`${selectedUser.firstName} ${selectedUser.lastName}`}
+            <div className={styles['btn-block']} onClick={() => setSelectedUser(undefined)}>
+              <MdCancel />
+            </div>
+          </div>
+        )}
       </div>
       <div className={styles['pop-up__btns-block']}>
         <div
-          className={styles['btn-block'] + ' ' + styles['btn-block--primary']}
+          className={`${styles['btn-block']} ${
+            selectedUser ? styles['btn-block--primary'] : styles['blocked']
+          }`}
           onClick={onSubmitHandler}>
           Add
         </div>
